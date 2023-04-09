f123e3430816def35023d01cf41ccf44d143d983
Hannah Beattie <hannahdbeattie@gmail.com>
2023-04-10 01:29:14 +1200

    add phone nav

--- public/components/nav/HorizontalHide.js
+++ public/components/nav/HorizontalHide.js
@@ -1,19 +1,43 @@
-import { Button, Heading, VStack, useDisclosure } from '@chakra-ui/react'
+import {
+	Button,
+	Heading,
+	Icon,
+	IconButton,
+	Image,
+	Text,
+	VStack,
+	useDisclosure,
+} from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import { useState } from 'react'
 import { items } from './Header'
+import CustomIcon from './CustomIcon'
+import { useRouter } from 'next/router'
+import StyledNextLink from './StyledNextLink'
 
 export default function HorizontalHide() {
 	const navItems = items
-	const { getButtonProps, getDisclosureProps, isOpen } = useDisclosure()
+	const { getButtonProps, getDisclosureProps, isOpen, onClose } = useDisclosure()
 	const [hidden, setHidden] = useState(!isOpen)
 
+	const handleClick = () => {
+		onClose()
+	}
+	const [rotate, setRotate] = useState(0)
+
 	return (
-		// {navItems.map(({ href, page, icon }, idx) => ()}
 		<VStack flex={1} alignItems={'stretch'} display={{ lg: 'none' }} spacing={0}>
-			<Button {...getButtonProps()} position={'absolute'} top={0}>
-				Toggle
+			<Button
+				{...getButtonProps()}
+				bg={'transparent'}
+				position={'absolute'}
+				top={2}
+				right={0}
+				borderRadius={200}
+			>
+				<Image h={8} src={'/assets/navcons/add.png'} alt={'add icon'} />{' '}
 			</Button>
+
 			<motion.div
 				{...getDisclosureProps()}
 				hidden={hidden}
@@ -22,20 +46,45 @@ export default function HorizontalHide() {
 				onAnimationComplete={() => setHidden(!isOpen)}
 				animate={{ width: isOpen ? '100vw' : 0 }}
 				style={{
-					background: '#000',
 					overflow: 'hidden',
 					whiteSpace: 'nowrap',
 					position: 'absolute',
 					right: '0',
-
 					zIndex: 2,
 					height: '100vh',
 					top: '0',
 				}}
 			>
-				<VStack flex={1}>
-					<Heading>welcome home</Heading>
-					<Button {...getButtonProps()}>Close</Button>
+				<VStack
+					flex={1}
+					backgroundImage={'/assets/bg/2.png'}
+					h={'100vh'}
+					w={'100vw'}
+					position={'fixed'}
+				>
+					<Button
+						position={'absolute'}
+						left={2}
+						top={2}
+						variant={'unstyled'}
+						onClick={handleClick}
+					>
+						<motion.div whileInView={{ rotate: 50 }}>
+							<Image h={8} src={'/assets/navcons/add.png'} alt={'add icon'} />
+						</motion.div>
+					</Button>
+					<VStack spacing={4} pt={100}>
+						<StyledNextLink href={'/'} onClick={handleClick}>
+							<Text fontSize={'2xl'}>home</Text>
+						</StyledNextLink>
+						{navItems.map(({ href, page, icon }, idx) => (
+							<Button key={`item-key${idx}`} variant={'unstyled'} flex={1}>
+								<StyledNextLink href={href} onClick={handleClick}>
+									<Text fontSize={'2xl'}>{page}</Text>
+								</StyledNextLink>
+							</Button>
+						))}
+					</VStack>
 				</VStack>
 			</motion.div>
 		</VStack>
